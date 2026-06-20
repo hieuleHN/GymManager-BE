11ce73ac3d47aa38ceeb45a3ca448d8ce9d3d46e
hieuleHN <lehieu1682002@gmail.com>
2026-06-20 19:46:23 +0700

    update ui thiet bi and add status

--- routes/equipmentRoutes.js
+++ routes/equipmentRoutes.js
@@ -3,22 +3,13 @@ import * as EquipmentController from '../controllers/equipmentController.js';
 
 const router = express.Router();
 
-// Lấy tất cả thiết bị
 router.get('/', EquipmentController.getAllEquipments);
-
-// Lấy thiết bị theo cơ sở (location)
 router.get('/location/:locationId', EquipmentController.getEquipmentsByLocation);
-
-// Lấy thiết bị theo ID
 router.get('/:id', EquipmentController.getEquipmentById);
-
-// Thêm thiết bị
 router.post('/', EquipmentController.createEquipment);
-
-// Cập nhật thiết bị
 router.put('/:id', EquipmentController.updateEquipment);
-
-// Xóa thiết bị
 router.delete('/:id', EquipmentController.deleteEquipment);
+router.post('/:id/report', EquipmentController.reportEquipment);
+router.put('/:id/report/:reportId/resolve', EquipmentController.resolveReport);
 
 export default router;
