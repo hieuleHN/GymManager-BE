eb5a7aa9dcf63199cff29cae28b15f9b4f86b9a6
huutoan950-coder <huutoan950@gmail.com>
2026-07-17 10:40:16 +0700

    mở public API lấy danh sách công việc cho form tuyển dụng

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,15 +1,20 @@
-import express from 'express';
-import { authenticateToken } from '../middleware/authMiddleware.js';
+import express from "express";
+import { authenticateToken } from "../middleware/authMiddleware.js";
 import {
-  list, detail, create, update, remove
-} from '../controllers/jobController.js';
+  list,
+  detail,
+  create,
+  update,
+  remove,
+} from "../controllers/jobController.js";
 
 const router = express.Router();
 
-router.get('/', authenticateToken, list);
-router.get('/:id', authenticateToken, detail);
-router.post('/', authenticateToken, create);
-router.put('/:id', authenticateToken, update);
-router.delete('/:id', authenticateToken, remove);
+router.get("/", list);
+
+router.get("/:id", authenticateToken, detail);
+router.post("/", authenticateToken, create);
+router.put("/:id", authenticateToken, update);
+router.delete("/:id", authenticateToken, remove);
 
 export default router;
