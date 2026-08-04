909571c70aa116aeafda802162bdfe38a470e9ca
huutoan950-coder <huutoan950@gmail.com>
2026-08-04 21:48:45 +0700

    Bổ sung hàm getGrowthData giả lập dữ liệu cho biểu đồ

--- v2/services/statisticsService.js
+++ v2/services/statisticsService.js
@@ -13,3 +13,17 @@ exports.calculateOverview = async () => {
     throw error;
   }
 };
+exports.getGrowthData = async () => {
+  try {
+    return {
+      labels: ["Tuần 1", "Tuần 2", "Tuần 3", "Tuần 4"],
+      datasets: [
+        { label: "Khách hàng mới", data: [15, 22, 18, 30] },
+        { label: "Booking thành công", data: [120, 150, 145, 180] },
+      ],
+    };
+  } catch (error) {
+    console.log("Lỗi gen data chart:", error);
+    throw error;
+  }
+};
